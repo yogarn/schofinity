a14d8fd70041d8574cc30db6837e546e89accb36
Yoga Raditya <radityanalaa@gmail.com>
2024-03-03 07:55:38 +0700

    chore: Add dependencies - dotenv v16.4.5 - express v4.18.3

---
+++ src/app.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+server.setTimeout(2000);
