8597445fcf7e2901048814e2bed44f350570ef8c
Yoga Raditya <radityanalaa@gmail.com>
2024-03-03 19:56:33 +0700

    chore: Add transaction support to Sequelize

--- src/services/user.js
+++ src/services/user.js
@@ -1,19 +1,26 @@
 const db = require('../models/index');
+const sequelize = require('../config/sequelize')
 const bcrypt = require('bcrypt');
 
 const { User, Role } = db;
 
 async function find() {
-    return User.findAll({
-        include: [{ model: Role }],
-        attributes: { exclude: ['roleId', 'password'] }
+    return sequelize.transaction(async (t) => {
+        return User.findAll({
+            include: [{ model: Role }],
+            attributes: { exclude: ['roleId', 'password'] }
+        }, { transaction: t });
     });
 };
 
 async function create(data) {
-    const { username, name, password, contact, profilePict, roleId } = data;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    return User.create({ username, password: hashedPassword, name, contact, profilePict, roleId });
+    return sequelize.transaction(async (t) => {
+        const { username, name, password, contact, profilePict, roleId } = data;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        return User.create({
+            username, password: hashedPassword, name, contact, profilePict, roleId
+        }, { transaction: t });
+    });
 };
 
 module.exports = {
