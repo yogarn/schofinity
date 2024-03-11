9b0d60b81144705046f46f5f77dbec9648577749
Yoga Raditya <radityanalaa@gmail.com>
2024-03-11 10:09:44 +0700

    chore: Create Workshop Sequelize model and migration

---
+++ src/migrations/20240311025646-create-workshop.js
@@ -0,0 +1,62 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Workshops', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      categoryId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Categories',
+          key: 'id',
+          as: 'categoryId',
+        },
+      },
+      mentorId: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        references: {
+          model: 'Mentors',
+          key: 'id',
+          as: 'mentorId',
+        },
+      },
+      startDate: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      endDate: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.DOUBLE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Workshops');
+  }
+};
---
+++ src/models/workshop.js
@@ -0,0 +1,50 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Workshop extends Model {
+    static associate(models) {
+      Workshop.belongsTo(models.Category, { foreignKey: 'categoryId', allowNull: false });
+      Workshop.belongsTo(models.Mentor, { foreignKey: 'mentorId', allowNull: false });
+    }
+  }
+  Workshop.init({
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    description: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    categoryId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    mentorId: {
+      allowNull: false,
+      type: DataTypes.UUID
+    },
+    startDate: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    endDate: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    price: {
+      allowNull: false,
+      type: DataTypes.DOUBLE
+    }
+  }, {
+    sequelize,
+    modelName: 'Workshop',
+  });
+  return Workshop;
+};
