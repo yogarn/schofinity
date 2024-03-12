bb91572c367d4e0fccc7f64b6fb7f063669570ce
Yoga Raditya <radityanalaa@gmail.com>
2024-03-12 22:15:42 +0700

    chore: Add cors

--- src/app.js
+++ src/app.js
@@ -5,9 +5,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const routes = require('./routes/index');
 app.use('/v1', routes);
