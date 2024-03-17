f112c1485de5cbad6937618db819143b956afb61
Yoga Raditya <radityanalaa@gmail.com>
2024-03-17 11:16:46 +0700

    fix: Fix error on midtrans endpoint

--- src/controllers/midtrans.js
+++ src/controllers/midtrans.js
@@ -5,31 +5,36 @@ const { sendResponse, sendError } = require('../services/responseHandler');
 const { clearEndpoints } = require('../services/cache');
 
 async function resolvePayments(req, res, next) {
-  midtrans.transaction.notification(req.body)
-    .then(async (statusResponse) => {
-      let orderId = statusResponse.order_id;
-      let transactionStatus = statusResponse.transaction_status;
-      let fraudStatus = statusResponse.fraud_status;
-      console.log(`Transaction notification received. Order ID: ${orderId}. Transaction status: ${transactionStatus}. Fraud status: ${fraudStatus}`);
+  try {
+    await midtrans.transaction.notification(req.body)
+      .then(async (statusResponse) => {
+        let orderId = statusResponse.order_id;
+        let transactionStatus = statusResponse.transaction_status;
+        let fraudStatus = statusResponse.fraud_status;
+        console.log(`Transaction notification received. Order ID: ${orderId}. Transaction status: ${transactionStatus}. Fraud status: ${fraudStatus}`);
 
-      const parts = orderId.split(" ");
-      const classType = parts[0];
-      const id = parts[1];
+        const parts = orderId.split(" ");
+        const classType = parts[0];
+        const id = parts[1];
 
-      try {
-        if (classType == "mentoring") {
-          await mentoringPayments(id, transactionStatus, fraudStatus);
-          sendResponse(res, orderId);
-          clearEndpoints(['/v1/mentorings']);
-        } else if (classType == "class") {
-          await classPayment(id, transactionStatus, fraudStatus);
-          clearEndpoints(['/v1/classes/payments']);
+        try {
+          if (classType == "mentoring") {
+            await mentoringPayments(id, transactionStatus, fraudStatus);
+            sendResponse(res, orderId);
+            clearEndpoints(['/v1/mentorings']);
+          } else if (classType == "class") {
+            await classPayment(id, transactionStatus, fraudStatus);
+            clearEndpoints(['/v1/classes/payments']);
+          }
+        } catch (e) {
+          console.log(e);
+          sendError(res, e.message);
         }
-      } catch (e) {
-        console.log(e);
-        sendError(res, e.message);
-      }
-    });
+      });
+  } catch (error) {
+    console.error("Error processing midtrans notification:", error);
+    sendError(res, "Error processing midtrans notification");
+  }
 }
 
 module.exports = resolvePayments;
