3cfe834f7811abb4c6892c126de692c35c96968d
Yoga Raditya <radityanalaa@gmail.com>
2024-03-18 06:17:19 +0700

    fix: Fix invalid jwt token

--- src/middlewares/authToken.js
+++ src/middlewares/authToken.js
@@ -1,18 +1,23 @@
 const jwt = require('jsonwebtoken');
 const { sendError } = require('../services/responseHandler');
+const { find, findByUserId } = require('../services/user');
 
-function authenticateToken(req, res, next) {
+async function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if (!token) {
         return sendError(res, 'No token provided');
     }
 
-    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+    jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, decoded) => {
         if (err) {
             return sendError(res, 'Invalid jwt token');
         } else {
             req.jwt = decoded;
+            const user = await findByUserId(req.jwt.userId);
+            if (!user) {
+                return sendError(res, 'Invalid jwt token');
+            }
             next();
         }
     });
--- src/routes/scholarship.js
+++ src/routes/scholarship.js
@@ -5,14 +5,14 @@ const authToken = require('../middlewares/authToken');
 const cache = require('../middlewares/cache');
 const upload = require('../config/multer');
 const { checkScholarshipOwnership, checkRoleId } = require('../middlewares/authorize');
-const { addValidate, editValidate } = require('../middlewares/validators/scholarship');
+const { addValidate, editValidate, idValidate } = require('../middlewares/validators/scholarship');
 
 router
     .get('/', cache.get, getAllScholarships, cache.set)
     .get('/:id', cache.get, getScholarship, cache.set)
     .post('/', upload.single('image'), authToken, addValidate, addScholarship, cache.clear)
-    .post('/:id/accept', authToken, checkRoleId([3]), acceptScholarship, cache.clear)
+    .post('/:id/accept', authToken, checkRoleId([3]), idValidate, acceptScholarship, cache.clear)
     .patch('/:id', upload.single('image'), authToken, checkScholarshipOwnership, editValidate, updateScholarship, cache.clear)
-    .delete('/:id', upload.single('image'), authToken, checkScholarshipOwnership, deleteScholarship, cache.clear);
+    .delete('/:id', upload.single('image'), authToken, checkScholarshipOwnership, idValidate, deleteScholarship, cache.clear);
 
 module.exports = router;
